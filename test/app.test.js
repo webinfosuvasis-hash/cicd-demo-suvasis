2bf27f3cd379ccac2b423711c8898764dbfe0dd9
G <goutam.nayek@webguru-development.com>
2025-12-05 10:47:25 +0530

    Updated

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ describe('GET /health', () => {
 it('responds with status ok', async () => {
 const res = await request(app).get('/health');
 expect(res.statusCode).toBe(200);
-console.log(res.body);
+console.log(res.body,'response body');
 expect(res.body).toHaveProperty('status', 'ok');
 });
 });
